42dd0171255c9f88c0cd54504f690472e9836f5a
miyabinu <miyabinu456@gmail.com>
2025-07-26 19:44:54 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -5,15 +5,5 @@ document.addEventListener("DOMContentLoaded", function () {
     </p>
   `;
 
-  // Find or create a container for the "About" section
-  let aboutSection = document.getElementById("about");
-
-  // If it doesn't exist, create it
-  if (!aboutSection) {
-    aboutSection = document.createElement("section");
-    aboutSection.id = "about";
-    document.body.appendChild(aboutSection);
-  }
-
   aboutSection.innerHTML = aboutContent;
 });
